80f35d4d925ea07fbecdfb40489bb300c6c0ee09
jdpc86 <jdpc86.li@gmail.com>
2021-03-24 16:19:30 -0400

    zlib compress/inflate

--- rsa/zlib_flate.py
+++ rsa/zlib_flate.py
@@ -1,10 +1,10 @@
 import zlib
 
-inputFile = '/Users/jd/tmpjd/archer/cfg_bin/tmp.zlib'
+inputFile = '/Users/jd/01_github/archerc9/config/tmp.zlib'
 with open(inputFile, 'rb') as f:
     # src = f.read().decode('utf8')
     src = f.read()
-outputFile = '/Users/jd/tmpjd/archer/cfg_bin/tmp.json'
+outputFile = '/Users/jd/tmpjd/archer/tmp_orig.json'
 with open(outputFile, "w+b") as fd:
     fd.write(zlib.decompress(src, 32))
     # os.chmod(private_key_filename, 0o600)
